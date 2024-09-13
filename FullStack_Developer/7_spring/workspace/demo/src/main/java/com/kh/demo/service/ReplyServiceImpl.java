d7bd7666b103b4724c24f3a7651163e7186e7de7
id235791 <85696884+id235791@users.noreply.github.com>
2024-09-13 17:38:37 +0900

    Update ReplyServiceImpl.java

--- FullStack_Developer/7_spring/workspace/demo/src/main/java/com/kh/demo/service/ReplyServiceImpl.java
+++ FullStack_Developer/7_spring/workspace/demo/src/main/java/com/kh/demo/service/ReplyServiceImpl.java
@@ -1,18 +1,20 @@
-package com.kh.demo.service;
+package com.example.demo.service;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.kh.demo.domain.dto.Criteria;
-import com.kh.demo.domain.dto.ReplyDTO;
-import com.kh.demo.domain.dto.ReplyPageDTO;
-import com.kh.demo.mapper.ReplyMapper;
+import com.example.demo.domain.Criteria;
+import com.example.demo.domain.ReplyDTO;
+import com.example.demo.domain.ReplyPageDTO;
+import com.example.demo.mapper.ReplyMapper;
 
 @Service
 public class ReplyServiceImpl implements ReplyService{
 	@Autowired
 	private ReplyMapper rmapper;
-
+	
 	@Override
 	public ReplyDTO regist(ReplyDTO reply) {
 		//등록 데이터 로직
@@ -22,29 +24,44 @@ public class ReplyServiceImpl implements ReplyService{
 		}
 		return null;
 	}
-
-	@Override
-	public ReplyDTO getDetail(long replynum) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
 	@Override
 	public ReplyPageDTO getList(Criteria cri, long boardnum) {
-		return new ReplyPageDTO(rmapper.getTotal(boardnum), rmapper.getList(cri, boardnum));
+//		return new ReplyPageDTO(rmapper.getTotal(boardnum), rmapper.getList(cri,boardnum));
+		
+		long totalCnt = rmapper.getTotal(boardnum);
+		List<ReplyDTO> list = rmapper.getList(cri,boardnum);
+		
+		ReplyPageDTO dto = new ReplyPageDTO(totalCnt, list);
+		
+		return dto;
 	}
-
-	@Override
-	public boolean modify(ReplyDTO reply) {
-		return rmapper.updateReply(reply) == 1;
-	}
-
+	
 	@Override
 	public boolean remove(long replynum) {
 		return rmapper.deleteReply(replynum) == 1;
 	}
 	
+	@Override
+	public boolean modify(ReplyDTO reply) {
+		ReplyDTO rdto = rmapper.getDetail(reply.getReplynum());
+		if(rdto.getUserid().equals(reply.getUserid())) {
+			return rmapper.updateReply(reply) == 1;
+		}
+		return false;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
